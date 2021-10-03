630a81cd785c3a6377a096b36b213ad780cc77aa
Akash <akashdas6881@gmail.com>
2021-10-04 03:00:48 +0530

    added the recommendation system basic prototype

--- recommendation_system/recommend/views.py
+++ recommendation_system/recommend/views.py
@@ -1,7 +1,31 @@
+import pandas as pd
 from django.shortcuts import render
 from django.http import HttpResponse
+import pickle
+
 
 # Create your views here.
 
+similarity = pickle.load(open('./ml_data/similarity.pkl', 'rb'))
+
+data = pd.read_csv('./ml_data/movie_data_with_tags.csv')
+
 def index(request):
-    return HttpResponse("ajbdhj")
+    return render(request, 'recommend/index.html')
+
+
+def recommend(request):
+    movie_name = request.GET.get('movie_name')
+
+    movies_index = data[data['title'] == movie_name].index[0]
+
+    distances = similarity[movies_index]
+
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:10]
+    s = ""
+    for i in movies_list:
+        s = s + data.iloc[i[0]].title + " "
+
+
+
+    return HttpResponse(s)
