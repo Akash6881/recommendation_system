73b1a9896534bf8efa59b2506c5f0d056bfc14b0
Akash <akashdas6881@gmail.com>
2021-10-04 21:09:22 +0530

    added api for poster and /movie_id page initial backend

--- recommendation_system/recommend/urls.py
+++ recommendation_system/recommend/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 from . import views
-
+app_name = 'recommend'
 urlpatterns = [
     path('', views.index, name = "home"),
     path('recommend/', views.recommend, name = "recommend"),
+    path('<int:movie_id>/', views.movie_details, name = 'movie_details')
 ]
--- recommendation_system/recommend/views.py
+++ recommendation_system/recommend/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import pickle
 
+import requests
+
+from .models import Search
+
 
 # Create your views here.
 
@@ -10,26 +14,51 @@ similarity = pickle.load(open('./ml_data/similarity.pkl', 'rb'))
 
 data = pd.read_csv('./ml_data/movie_data_with_tags.csv')
 
+def fetch(movie_id):
+    URL = 'https://api.themoviedb.org/3/movie/{}?api_key=6aeea4409e82ad77bfbe5f7beb516560&language=en-US'.format(movie_id)
+    response = requests.get(URL)
+    data = response.json()
+    return data
+
 def index(request):
     return render(request, 'recommend/index.html')
 
 
 def recommend(request):
     movie_name = request.GET.get('movie_name')
-
+    search = Search(search_content = movie_name)
+    search.save()
     movies_index = data[data['title'] == movie_name].index[0]
+
+
 
     distances = similarity[movies_index]
 
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:10]
-    s = []
+
+    recommend_movies = []
     for i in movies_list:
-        s.append(data.iloc[i[0]].title)
+
+        movie_id = data.iloc[i[0]].id
+        movie_details = fetch(movie_id)
+
+        poster_path = 'https://image.tmdb.org/t/p/w500'+ movie_details['poster_path']
+
+        dict = {
+            'movie_name' : data.iloc[i[0]].title,
+            'movie_id' : data.iloc[i[0]].id,
+            'movie_poster' : poster_path,
+        }
+        recommend_movies.append(dict)
+
 
 
     stuff_for_frontend = {
-        'recommends' : s
+        'recommends' : recommend_movies,
     }
 
 
     return render(request, 'recommend/recommend.html', stuff_for_frontend)
+
+def movie_details(reuqust, movie_id):
+    return HttpResponse(movie_id)
